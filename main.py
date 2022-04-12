a9432fc6e0d0dd5b3e8a63c73682edc7254a3dcf
nikwok <77155696+NIKITAvinogradovs@users.noreply.github.com>
2022-04-12 06:25:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,420 @@
+print("Atvert durvis bez rokam nav viegli un tagad mes pateiksim ka to izdarit")
+print("Atcereties! 1= ja, 2= ne")
+a = int (input("Vai jums ir problemas ar durvis atveršanu?"))
+if a==2:
+  print("Tad jums nav problemas ar durvis atveršanai")
+elif a==1:
+  print("Tad lasiet musu tutorialu")
+  b=int(input("Pirmais variants: vai jusu augums ir vairak neka 160 "))
+  if b==1:
+    print ("Atveriet durvis ar kaju")
+    c = int(input("Šis variants palidzeja?"))
+    if c ==1:
+      print("Ceram ka jums vairak nebus problemas ar durvim")
+    elif c ==2:
+      print ("Tad nakamais variants")
+      c1= int (input("jums ir atverts logs? "))
+      if c1==1:
+        c2=int(input("Jus dzivojat uz pirma stava?"))
+        if c2==1:
+          print("Lecat no loga")
+          c = int(input("Šis variants palidzeja?"))
+          if c ==1:
+            print("Ceram ka jums vairak nebus problemas ar durvim")
+          elif c ==2:
+            print ("Tad nakamais variants")
+            e4=int(input("Jus esat dzivokļa viens?"))
+            if e4 ==1:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                  print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+            elif e4 ==2:
+              e5=int(input("paprasiet cita cilveka atvert durvis"))
+              c = int(input("Šis variants palidzeja?"))
+              if c ==1:
+                print("Ceram ka jums vairak nebus problemas ar durvim")
+              elif c ==2:
+                print ("Tad nakamais variants")
+                u=int(input("Vai jums tiešam vaig iziet no dzivokļa?"))
+                if u ==1:
+                  u2= int(input("Jus esat bagatie?"))
+                  if u2 ==1:
+                    print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                  elif u2 ==2:
+                    print("ne varam jums palidzet")
+                elif u==2:
+                  print("tad sežiet majā")
+
+
+
+        elif c2==2:
+          print("Tad nakamais variants")
+          e4=int(input("Jus esat dzivokļa viens?"))
+          if e4 ==1:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+          elif e4 ==2:
+            e5=int(input("paprasiet cita cilveka atvert durvis"))
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                   print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+
+      elif c1==2:
+        c9=int(input("jus varat atvert logu? "))
+        if c9==1:
+          c2=int(input("Jus dzivojat uz pirma stava?"))
+          if c2==1:
+            print("Lecat no loga")
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              e4=int(input("Jus esat dzivokļa viens?"))
+              if e4 ==1:
+                print ("Tad nakamais variants")
+                u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+                if u ==1:
+                  u2= int(input("Jus esat bagatie?"))
+                  if u2 ==1:
+                    print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                  elif u2 ==2:
+                    print("ne varam jums palidzet")
+                elif u==2:
+                  print("tad sežiet majā")
+              elif e4 ==2:
+                e5=int(input("paprasiet cita cilveka atvert durvis"))
+                c = int(input("Šis variants palidzeja?"))
+                if c ==1:
+                  print("Ceram ka jums vairak nebus problemas ar durvim")
+                elif c ==2:
+                  print ("Tad nakamais variants")
+                  u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+                  if u ==1:
+                    u2= int(input("Jus esat bagatie?"))
+                    if u2 ==1:
+                      print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                    elif u2 ==2:
+                      print("ne varam jums palidzet")
+                  elif u==2:
+                    print("tad sežiet majā")
+
+
+
+          elif c2==2:
+            print ("Tad nakamais variants")
+            e4=int(input("Jus esat dzivokļa viens?"))
+            if e4 ==1:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                  print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+            elif e4==2:
+              e5=int(input("paprasiet cita cilveka atvert durvis"))
+              c = int(input("Šis variants palidzeja?"))
+              if c ==1:
+                print("Ceram ka jums vairak nebus problemas ar durvim")
+              elif c ==2:
+                print ("Tad nakamais variants")
+                u=int(input("Vai jums tiešam vaig iziet no dzivokļa?"))
+                if u ==1:
+                  u2= int(input("Jus esat bagatie?"))
+                  if u2 ==1:
+                    print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                  elif u2 ==2:
+                    print("ne varam jums palidzet")
+                elif u==2:
+                  print("tad sežiet majā")
+        elif c9 ==2:
+          print ("Tad nakamais variants")
+          e4=int(input("Jus esat dzivokļa viens?"))
+          if e4 ==1:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+          elif e4 ==2:
+            e5=int(input("paprasiet cita cilveka atvert durvis"))
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                   print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+
+        elif c2==2:
+          print("Tad nakamais variants")
+          e4=int(input("Jus esat dzivokļa viens?"))
+          if e4 ==1:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+          elif e4 ==2:
+            e5=int(input("paprasiet cita cilveka atvert durvis"))
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                   print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+  elif b==2:
+    print ("Tad nakamais variants")
+    c1= int (input("jums ir atverts logs? "))
+    if c1==1:
+      c2=int(input("Jus dzivojat uz pirma stava?"))
+      if c2==1:
+        print("Lecat no loga")
+        c = int(input("Šis variants palidzeja?"))
+        if c ==1:
+         print("Ceram ka jums vairak nebus problemas ar durvim")
+        elif c ==2:
+          print ("Tad nakamais variants")
+          e4=int(input("Jus esat dzivokļa viens?"))
+          if e4 ==1:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+          elif e4 ==2:
+            e5=int(input("paprasiet cita cilveka atvert durvis"))
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                  print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+      elif c2==2:
+        print("Tad nakamais variants")
+        e4=int(input("Jus esat dzivokļa viens?"))
+        if e4 ==1:
+          print ("Tad nakamais variants")
+          u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+          if u ==1:
+            u2= int(input("Jus esat bagatie?"))
+            if u2 ==1:
+              print("tad nopirkiet protezus un patstavigi atveriet durvis")
+            elif u2 ==2:
+              print("ne varam jums palidzet")
+          elif u==2:
+            print("tad sežiet majā")
+        elif e4 ==2:
+          e5=int(input("paprasiet cita cilveka atvert durvis"))
+          c = int(input("Šis variants palidzeja?"))
+          if c ==1:
+            print("Ceram ka jums vairak nebus problemas ar durvim")
+          elif c ==2:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                  print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+    elif c1==2:
+      c9=int(input("jus varat atvert logu? "))
+      if c9==1:
+        c2=int(input("Jus dzivojat uz pirma stava?"))
+        if c2==1:
+          print("Lecat no loga")
+          c = int(input("Šis variants palidzeja?"))
+          if c ==1:
+            print("Ceram ka jums vairak nebus problemas ar durvim")
+          elif c ==2:
+            print ("Tad nakamais variants")
+            e4=int(input("Jus esat dzivokļa viens?"))
+            if e4 ==1:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                  print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+            elif e4 ==2:
+              e5=int(input("paprasiet cita cilveka atvert durvis"))
+              c = int(input("Šis variants palidzeja?"))
+              if c ==1:
+                print("Ceram ka jums vairak nebus problemas ar durvim")
+              elif c ==2:
+                print ("Tad nakamais variants")
+                u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+                if u ==1:
+                  u2= int(input("Jus esat bagatie?"))
+                  if u2 ==1:
+                    print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                  elif u2 ==2:
+                    print("ne varam jums palidzet")
+                elif u==2:
+                  print("tad sežiet majā")
+        elif c2==2:
+          print ("Tad nakamais variants")
+          e4=int(input("Jus esat dzivokļa viens?"))
+          if e4 ==1:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")          
+          elif e4 ==2:
+            e5=int(input("paprasiet cita cilveka atvert durvis"))
+            c = int(input("Šis variants palidzeja?"))
+            if c ==1:
+              print("Ceram ka jums vairak nebus problemas ar durvim")
+            elif c ==2:
+              print ("Tad nakamais variants")
+              u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+              if u ==1:
+                u2= int(input("Jus esat bagatie?"))
+                if u2 ==1:
+                  print("tad nopirkiet protezus un patstavigi atveriet durvis")
+                elif u2 ==2:
+                  print("ne varam jums palidzet")
+              elif u==2:
+                print("tad sežiet majā")
+            
+      elif c9 ==2:
+        print ("Tad nakamais variants")
+        e4=int(input("Jus esat dzivokļa viens?"))
+        if e4 ==1:
+          print ("Tad nakamais variants")
+          u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+          if u ==1:
+            u2= int(input("Jus esat bagatie?"))
+            if u2 ==1:
+              print("tad nopirkiet protezus un patstavigi atveriet durvis")
+            elif u2 ==2:
+              print("ne varam jums palidzet")
+          elif u==2:
+            print("tad sežiet majā")
+        elif e4 ==2:
+          e5=int(input("paprasiet cita cilveka atvert durvis"))
+          c = int(input("Šis variants palidzeja?"))
+          if c ==1:
+            print("Ceram ka jums vairak nebus problemas ar durvim")
+          elif c ==2:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                  print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
+      elif c2==2:
+        print("Tad nakamais variants")
+        e4=int(input("Jus esat dzivokļa viens?"))
+        if e4 ==1:
+          print ("Tad nakamais variants")
+          u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+          if u ==1:
+            u2= int(input("Jus esat bagatie?"))
+            if u2 ==1:
+              print("tad nopirkiet protezus un patstavigi atveriet durvis")
+            elif u2 ==2:
+              print("ne varam jums palidzet")
+          elif u==2:
+            print("tad sežiet majā")
+        elif e4 ==2:
+          e5=int(input("paprasiet cita cilveka atvert durvis"))
+          c = int(input("Šis variants palidzeja?"))
+          if c ==1:
+            print("Ceram ka jums vairak nebus problemas ar durvim")
+          elif c ==2:
+            print ("Tad nakamais variants")
+            u=int(input("Vai vums tiešam vaig iziet no dzivokļa?"))
+            if u ==1:
+              u2= int(input("Jus esat bagatie?"))
+              if u2 ==1:
+                print("tad nopirkiet protezus un patstavigi atveriet durvis")
+              elif u2 ==2:
+                  print("ne varam jums palidzet")
+            elif u==2:
+              print("tad sežiet majā")
